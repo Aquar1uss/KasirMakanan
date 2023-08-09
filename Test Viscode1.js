caa77ae04964eff5053861e5244747d5cbaa0218
Aquar1uss <141600122+Aquar1uss@users.noreply.github.com>
2023-08-09 09:00:51 +0700

    Add files via upload

---
+++ Test Viscode1.js
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printin("Menu Makanan: ")
+        System.out.printin("1. Soto Ayam (15000)")
+        System.out.printin("2. Rawon (17000)")
+        System.out.printin("3. Mie (5000)")
+        System.out.printin("4. Tahu (8000)")
+        System.out.printin("5. sate (20000)")
+
+        int jumlahBeli,total,bayar,kembali;
+
+        Scanner input = new scanner System
+    
+}
